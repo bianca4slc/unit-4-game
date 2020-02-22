8a7f8194cb21a058c0399da738533188796aa952
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-21 23:13:25 -0700

    added images and pseudo code to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// four crystals with random hidden value between 1-12
+// show random number at start of game between 12-120
+// crystal click = adds points to player score
+// update player score counter with each click
+// player must match their score to random number to win
+// player loses if score goes above random number
+// game resets at win or loss
+// random values refreshed when game is reset
+// track total wins/losses
+// do not refresh page to restart game
