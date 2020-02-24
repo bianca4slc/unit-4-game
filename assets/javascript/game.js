21fc20c73ef599aeaf8f83ebd5c706df6af2a464
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-24 13:47:36 -0700

    added image, completed html and starting on js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
+$(document).ready(function() {
+
+});
 // four crystals with random hidden value between 1-12
+var crystals = ["red", "yellow", "purple", "heart"];
+
 // show random number at start of game between 12-120
+var minNumber = 12;
+var maxNumber = 120;
+var randomNumber = fromRange(minNumber, maxNumber); 
+
+function fromRange(min,max) {
+    return Math.floor(Math.random()*(min,max+1)+min);
+    document.getElementById("random-number").innerText = randomNumber
+
+    console.log(randomNumber)
+}
 // crystal click = adds points to player score
 // update player score counter with each click
 // player must match their score to random number to win
