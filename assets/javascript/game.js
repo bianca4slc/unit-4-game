5fb41ed05f2612d4f42217ec58886b4eb129f259
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-24 23:02:53 -0700

    simplified functions for random number to start and for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,57 @@
-$(document).ready(function() {
-
-});
+$(document).ready(function() {});
 // four crystals with random hidden value between 1-12
 var crystals = ["red", "yellow", "purple", "heart"];
+var crystal1 = crystalNumber();
+var crystal2 = crystalNumber();
+var crystal3 = crystalNumber();
+var crystal4 = crystalNumber();
+var crystalTotal = 0;
+var Wins = 0;
+var Losses = 0;
 
-// show random number at start of game between 12-120
-var minNumber = 12;
-var maxNumber = 120;
-var randomNumber = fromRange(minNumber, maxNumber); 
-
-function fromRange(min,max) {
-    return Math.floor(Math.random()*(min,max+1)+min);
-    document.getElementById("random-number").innerText = randomNumber
-
-    console.log(randomNumber)
+function crystalNumber() {
+  return Math.floor(Math.random() * (1, 12 + 1));
 }
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * (12, 120 + 1) + 12);
+  }
+
+$("#random-number").text(getRandomNumber());
+
 // crystal click = adds points to player score
 // update player score counter with each click
 // player must match their score to random number to win
 // player loses if score goes above random number
+$(".crystal-options").on("click", "img", function() {
+  var clickedCrystal = $(this).attr("id");
+
+  if (clickedCrystal === "red") {
+    crystalTotal = crystalTotal + crystal1;
+  } else if (clickedCrystal === "heart") {
+    crystalTotal = crystalTotal + crystal2;
+  } else if (clickedCrystal === "yellow") {
+    crystalTotal = crystalTotal + crystal3;
+  } else if (clickedCrystal === "purple") {
+    crystalTotal = crystalTotal + crystal4;
+  }
+  console.log(crystalTotal);
+  $("#score").text(crystalTotal);
+
+  // track total wins/losses
+  if (crystalTotal === getRandomNumber()) {
+    Wins = Wins + 1;
+    $("#wins").text(Wins);
+    // function to reset game
+  }
+
+  if (crystalTotal > getRandomNumber()) {
+    Losses = Losses + 1;
+    $("#losses").text(Losses);
+  }
+});
+
+
 // game resets at win or loss
 // random values refreshed when game is reset
-// track total wins/losses
 // do not refresh page to restart game
